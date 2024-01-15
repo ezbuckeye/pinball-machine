6028364dd50229da3f914fd90fb870000a5a8ef4
lu.2700 <lu.2700@cse-sl1.coeit.osu.edu>
2024-01-15 17:21:28 -0500

    finished lab2 with graphics

--- lab2/output.c
+++ lab2/output.c
@@ -17,8 +17,9 @@ void print_tab(double* ball, unsigned char ch, double time) {
 	if(GRAPHICS) {
 		pb_clear();
 		pb_ball(get_color(ch), ball[CX], ball[CY]);
+		pb_time((int)(time*1000));
 		pb_refresh();
-		microsleep(DELTA_T*1000000);
+		microsleep((int)(DELTA_T*1000000));
 	}
 }
 
@@ -27,17 +28,26 @@ void print_msg_off_table(double* ball, unsigned char ch) {
 		printf("Off table: ");
 		print_ST_X_Y_VX_VY(ball, ch);
 	}
+	if(GRAPHICS) {
+		pb_status("Off table");
+	}
 }
 
 void print_msg_loaded(double* ball, unsigned char ch) {
 	if(TEXT) {
 		printf("Loaded %02hhX %d ball at %.5f, %.5f %.5f deg %.5f ips\n", ch, get_color(ch), ball[CX], ball[CY], ball[THETA], ball[FORCE]);
 	}
+	if(GRAPHICS) {
+		pb_status("Loaded");
+	}
 }
 
 void print_msg_launched(double* ball, unsigned char ch) {
 	if(TEXT) {
 		printf("Launched %02hhX %d ball at %.5f, %.5f at %.5f, %.5f\n", ch, get_color(ch), ball[CX], ball[CY], ball[VX], ball[VY]);
+	}
+	if(GRAPHICS) {
+		pb_status("Launch");
 	}
 }
 
@@ -46,6 +56,9 @@ void print_msg_L_wall(double* ball, unsigned char ch) {
 		printf("Left_ wall: ");
 		print_ST_X_Y_VX_VY(ball, ch);
 	}
+	if(GRAPHICS) {
+		pb_status("Left_ wall");
+	}
 }
 
 void print_msg_R_wall(double* ball, unsigned char ch) {
@@ -53,11 +66,16 @@ void print_msg_R_wall(double* ball, unsigned char ch) {
 		printf("Right wall: ");
 		print_ST_X_Y_VX_VY(ball, ch);
 	}
-}
-
+	if(GRAPHICS) {
+		pb_status("Right wall");
+	}
+} 
 void print_msg_T_wall(double* ball, unsigned char ch) {
 	if(TEXT){
 		printf("Upper wall: ");
 		print_ST_X_Y_VX_VY(ball, ch);
+	}
+	if(GRAPHICS) {
+		pb_status("Upper wall");
 	}
 }
